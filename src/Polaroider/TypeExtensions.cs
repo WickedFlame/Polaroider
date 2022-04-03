d7ed6aa0638283f4d1d53def1085b07ec047c8e0
Christian Walpen <christianwalpen@opacc.ch>
2022-04-03 21:28:39 +0200

    Creating snapshots from a thread caused an error

---
+++ src/Polaroider/TypeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Polaroider
+{
+    internal static class TypeExtensions
+    {
+        public static bool IsAnonymous(this MethodInfo method)
+        {
+            var invalidChars = new[] { '<', '>' };
+            return method.Name.Any(invalidChars.Contains);
+        }
+    }
+}
