9981bfd1336cac19e067a69fe82eb3deb1076b0a
Christian Walpen <christianwalpen@opacc.ch>
2023-01-17 16:10:59 +0100

    Try to create som test that will be optimized by the controller

--- src/Tests/Polaroider.Tests/ReleaseOptimizerTests.cs
+++ src/Tests/Polaroider.Tests/ReleaseOptimizerTests.cs
@@ -1,35 +1,53 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Polaroider.Tests
 {
-    public class ReleaseOptimizerTests
+    public class ReleaseOptimizerTests : BaseClass
     {
+        //
+        // This test does not work
+        // We try to create some code that will be optimized by the compiler to test the TestMethodNotFoundException
+        // Until now: We failed...
+        //
+        // If we succeed this has to be moved to a new Testproject so the Optimize code option can be activated
+        //
+
         [Test]
-        public void ReleaseOptimizer_Check()
+        [TestCaseSource(nameof(GetData))]
+        public void ReleaseOptimizer_Check(string value)
         {
-            GetData().ToDictExt().MatchSnapshot();
+            RunTests(value);
         }
 
-        public IEnumerable<string> GetData()
+        public static IEnumerable<string> GetData
         {
-            for (int i = 0; i < 10; i++)
+            get
             {
-                if (true) 
-                    yield return i.ToString();
+                for (int i = 0; i < 10; i++)
+                {
+                    if (true)
+                        yield return i.ToString();
+                }
             }
+        }
+    }
+
+    public class BaseClass
+    {
+
+        public void RunTests(string str)
+        {
+            str.ToDictExt().MatchSnapshot(() => new { id = str[0] }, SnapshotOptions.Create(o => {}));
         }
     }
 
     public static class TestExtensions
     {
-        public static Dictionary<string, string> ToDictExt(this IEnumerable<string> values)
+        public static Dictionary<char, object> ToDictExt(this IEnumerable<char> values)
         {
-            return values.ToDictionary(s => s, s => s);
+            return values.ToDictionary<char, char, object>(s => s, o => new { Name = o });
         }
     }
 }
