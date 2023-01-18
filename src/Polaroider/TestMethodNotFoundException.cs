96dc84b0a44d2e43f3505a1aa331b6931e76440c
Christian Walpen <christianwalpen@opacc.ch>
2023-01-18 08:20:33 +0100

    Typo in exception

--- src/Polaroider/TestMethodNotFoundException.cs
+++ src/Polaroider/TestMethodNotFoundException.cs
@@ -17,7 +17,7 @@ namespace Polaroider
                 .AppendLine("Polaroider could not find the file containing the Test.")
                 .AppendLine("Please ensure the following is configured in the Testproject:")
                 .AppendLine("- Enable the generation of *.pdb files")
-                .AppendLine("- Disable Optimize code for build. This can be set in the Projetc Properties or through <Optimize>False</Optimize> in the *.csproj file of the Testproject")
+                .AppendLine("- Disable Optimize code for build. This can be set in the Project Properties or through <Optimize>False</Optimize> in the *.csproj file of the Testproject")
                 .AppendLine("- When using Live Unit Testing make sure the checkbox for 'Enable debug symbol and xml documentation comment generation' is enabled in the Visual Studio Options.");
 
             Message = msg.ToString();
