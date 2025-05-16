06e6ccbba033af64b8e16d6dc14e36bcbb8cfe03
Christian Walpen <c.walpen@gmx.net>
2025-05-16 10:27:19 +0200

    Update Version

--- build/Build.cs
+++ build/Build.cs
@@ -31,7 +31,7 @@ class Build : NukeBuild
     [GitRepository] readonly GitRepository GitRepository;
 
     [Parameter("Version to be injected in the Build")]
-    public string Version { get; set; } = $"2.4.0";
+    public string Version { get; set; } = $"2.4.1";
 
     [Parameter("The Buildnumber provided by the CI")]
     public int BuildNo = 1;
