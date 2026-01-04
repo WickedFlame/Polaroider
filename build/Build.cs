b23854f1d41f179d4bce1f81c7613fca1901eaa6
Christian Walpen <c.walpen@gmx.net>
2026-01-04 12:34:51 +0100

    Update to .NET10.0

--- build/Build.cs
+++ build/Build.cs
@@ -99,11 +99,13 @@ class Build : NukeBuild
                 foreach (var file in Directory.GetFiles(RootDirectory, $"*.{PackageVersion}.nupkg", SearchOption.AllDirectories))
                 {
                     ((AbsolutePath) file).CopyToDirectory(DeployPath, ExistsPolicy.FileOverwrite);
+                    Serilog.Log.Write(Serilog.Events.LogEventLevel.Information, "Deployed {0} to {1}", file, DeployPath);
                 }
 
                 foreach (var file in Directory.GetFiles(RootDirectory, $"*.{PackageVersion}.snupkg", SearchOption.AllDirectories))
                 {
                     ((AbsolutePath) file).CopyToDirectory(DeployPath, ExistsPolicy.FileOverwrite);
+                    Serilog.Log.Write(Serilog.Events.LogEventLevel.Information, "Deployed {0} to {1}", file, DeployPath);
                 }
             }
         );
