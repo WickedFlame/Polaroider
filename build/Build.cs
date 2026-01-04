27ad9e831c3c258d8dfb43aecf9b3190b73e050c
Christian Walpen <c.walpen@gmx.net>
2026-01-04 12:49:03 +0100

    Update dependencies

--- build/Build.cs
+++ build/Build.cs
@@ -34,7 +34,7 @@ class Build : NukeBuild
     public string Version { get; set; } = $"2.4.1";
 
     [Parameter("The Buildnumber provided by the CI")]
-    public int BuildNo = 1;
+    public int BuildNo = 3;
 
     [Parameter("Is RC Version")]
     public bool IsRc = false;
